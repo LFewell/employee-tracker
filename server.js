394c2ebd95cbcafa42fe3df1b891452d4c1a1fb5
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-10-19 01:51:34 -0400

    Added functionality to add employees to a table in the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,3 +18,57 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
+
+const init = () => {
+    console.log("Employee Manager ASCII Art")
+    inquirer.prompt(
+        {
+            name: "intro",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+        }
+    )
+    .then((answer) => {
+        if (answer.intro === "Add Employee"){
+            addEmployee();
+        }
+    }) 
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+        },
+        {
+            name: "roleId",
+            type: "number",
+            message: "What is the employee's role number?"
+        }
+    ])
+    .then((answer) => {
+        db.query(
+            "INSERT INTO employee SET ?",
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.roleId
+                
+            }
+        )
+    })
+}
+
+init();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
